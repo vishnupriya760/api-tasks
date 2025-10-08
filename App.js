4ad6fcb5e9dc07939cd25b3decc06f8be488db2c
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-10-08 17:31:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FormPage from "./components/FormPage";
+import DisplayData from "./components/DisplayData";
+
+function App() {
+  return (
+    <div>
+      <FormPage />
+      <DisplayData />
+    </div>
+  );
+}
+
+export default App;
